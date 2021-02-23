2a1aa4cd3dfe4edd58676a74afbe58d6b2af0366
Viktor Kushnir <mustbefail@gmail.com>
2021-02-23 14:27:58 +0200

    Fixed greetings message

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,9 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-  const userName = readlineSync.question('May I have your name? ');
-
   console.log('Welcome to the Brain Games!');
 
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 
   return userName;
