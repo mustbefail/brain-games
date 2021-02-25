4b803ba347a30dff583570d52cb76e27aedc0d4d
Viktor Kushnir <mustbefail@gmail.com>
2021-02-25 02:59:43 +0200

    fix brain-even game and asciinema link

--- src/even.js
+++ src/even.js
@@ -1,26 +1,29 @@
 import readlineSync from 'readline-sync';
 
 const isEven = (num) => num % 2 === 0;
-const toYesNoTranslate = (bool) => (bool ? 'yes' : 'no');
+const translateToYesNo = (bool) => (bool ? 'yes' : 'no');
 
-const randomNumber = () => Math.floor(Math.random() * 10);
+const getRandomNumber = () => Math.floor(Math.random() * 100);
 
 export default (userName) => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   let roundNumber = 0;
   while (roundNumber < 3) {
-    const rNumber = randomNumber();
-    console.log(`Question: ${rNumber}`);
-    const userInput = readlineSync.question('Your answer: ');
-    const correctAnswer = toYesNoTranslate(isEven(rNumber));
+    const randomNumber = getRandomNumber();
 
-    if (userInput === toYesNoTranslate(isEven(rNumber))) {
+    console.log(`Question: ${randomNumber}`);
+
+    const userInput = readlineSync.question('Your answer: ');
+    const correctAnswer = translateToYesNo(isEven(randomNumber));
+
+    if (userInput === translateToYesNo(isEven(randomNumber))) {
       roundNumber += 1;
       console.log('Correct!');
     } else {
       console.log(`${userInput} is wrong answer ;(.
-        Correct answer was '${correctAnswer}'.`);
+Correct answer was '${correctAnswer}'.`);
       console.log("Let's try again, Bill!");
+      return;
     }
   }
   console.log(`Congratulations, ${userName}`);
