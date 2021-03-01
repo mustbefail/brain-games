7b65a274ff881982d25612177a3bdeaad6c91961
Viktor Kushnir <mustbefail@gmail.com>
2021-03-02 00:03:41 +0200

    add constants insted magic numbers

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,20 +2,23 @@ import readlineSync from 'readline-sync';
 import { getRandomNumber } from '../helpers.js';
 
 const progressionLength = 10;
+const maxStep = 5;
+const maxProgrStep = 50;
+const maxSecretIdx = 10;
 
 export default () => {
   const progressionArray = [];
-  const progressionStep = getRandomNumber(5);
-  let progressionStart = getRandomNumber(50);
-  const secretIndex = getRandomNumber(10);
+  const progressionStep = getRandomNumber(maxStep);
+  let progressionStart = getRandomNumber(maxProgrStep);
+  const secretIndex = getRandomNumber(maxSecretIdx);
 
   for (let i = 0; i < progressionLength; i += 1, progressionStart += progressionStep) {
     progressionArray.push(progressionStart);
   }
 
   const correctAnswer = progressionArray[secretIndex];
-
   progressionArray[secretIndex] = '..';
+
   const question = progressionArray.join(' ');
   console.log(`Question: ${question}`);
 
